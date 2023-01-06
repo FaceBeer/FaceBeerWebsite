c0fb6f9a9a0574d87d6d9f71b6acdbffd9964574
GrantPerkins <gcperk20@gmail.com>
2023-01-06 17:09:28 -0500

    Format

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,65 +1,64 @@
-import React from 'react';
+import React from "react";
 import {
-    createTheme,
-    makeStyles,
-    ThemeProvider,
+  createTheme,
+  makeStyles,
+  ThemeProvider,
 } from "@material-ui/core/styles";
 import { CssBaseline, Typography } from "@material-ui/core";
 
-import Leaderboard from '../leaderboard/Leaderboard';
-import {BottomNavigation, Paper} from "@mui/material";
+import Leaderboard from "../leaderboard/Leaderboard";
+import { BottomNavigation, Paper } from "@mui/material";
 
 const theme = createTheme({
-    palette: {
-        text: {
-            primary: "#ffffff",
-        },
-        primary: {
-            main: "#ffffff",
-        },
-        background: {
-            default: "#3a60f1",
-        },
-        secondary: {
-            main: "#8aa0f1",
-        },
+  palette: {
+    text: {
+      primary: "#ffffff",
     },
+    primary: {
+      main: "#ffffff",
+    },
+    background: {
+      default: "#3a60f1",
+    },
+    secondary: {
+      main: "#8aa0f1",
+    },
+  },
 });
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        padding: "2%",
-    },
-    title: {
-        display: "inline-flex",
-        justifyContent: "center",
-        width: "100%",
-        padding: "2%",
-    },
+  root: {
+    padding: "2%",
+  },
+  title: {
+    display: "inline-flex",
+    justifyContent: "center",
+    width: "100%",
+    padding: "2%",
+  },
 }));
 
 function App() {
-    const classes = useStyles();
-    return (
-        <ThemeProvider theme={theme}>
-            <div className={classes.root}>
-                <CssBaseline />
-                <Typography
-                    className={classes.title}
-                    variant={"h1"}
-                    color={"primary"}
-                >
-                    FaceBeer
-                </Typography>
-                <Leaderboard />
-            </div>
-            <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
-                <Typography variant={"body1"} align="center">
-                    Copyright FaceBeer {new Date().getFullYear().toString()}
-                </Typography>
-            </Paper>
-        </ThemeProvider>
-    );
+  const classes = useStyles();
+  return (
+    <ThemeProvider theme={theme}>
+      <div className={classes.root}>
+        <CssBaseline />
+        <Typography className={classes.title} variant={"h1"} color={"primary"}>
+          FaceBeer
+        </Typography>
+        <Leaderboard />
+      </div>
+      <Paper
+        sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
+        elevation={3}
+      >
+        <Typography variant={"body1"} align="center">
+          Copyright FaceBeer {new Date().getFullYear().toString()}
+        </Typography>
+      </Paper>
+    </ThemeProvider>
+  );
 }
 
 export default App;
